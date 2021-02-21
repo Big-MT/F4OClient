44579faeda14edd4dd2214698ff0088f76b859e5
alin <alinoctavian@protonmail.com>
2021-02-21 20:44:51 +0200

    update CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Windows.h>
+
+DWORD WINAPI Attach(LPVOID lpThreadParameter) {
+
+
+}
+
+BOOL WINAPI Detach(){
+
+	return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved){
+
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls( hModule );
+
+		if ( auto handle = CreateThread( nullptr, NULL, Attach, hModule, NULL, nullptr ) )
+		{
+			CloseHandle( handle );
+		}
+	}else if(ul_reason_for_call == DLL_PROCESS_DETACH && !lpReserved){
+		return Detach();
+	}
+
+	return TRUE;
+}
